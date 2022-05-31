9cd5e6e35f4a37f966c7932eccb4300de76c6b7e
Loooooan <84080826+Loooooan@users.noreply.github.com>
2022-05-31 13:57:28 +0200

    Create Excel to Python.py

---
+++ Expérience pratique/Excel to Python.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+
+file = open("donne.txt", "r")
+data = file.readlines()
+
+T = []
+X_sans_TMD = []
+X_avec_TMD = []
+
+for e in data:
+    L = e.split(";")
+
+    T.append(int(L[0]))
+    X_sans_TMD.append(int(L[1]))
+    X_avec_TMD.append(int(L[2].strip()))
+
+#Vérification que toutes les données sont correctes
+print("Valeurs de temps")
+print(T)
+print("Valeurs sans TMD")
+print(X_sans_TMD)
+print("Valeurs avec TMD")
+print(X_avec_TMD)
+
+#Affichage
+plt.plot(T, X_sans_TMD)
+plt.plot(T, X_avec_TMD)
+
+plt.legend(["Sans TMD", "Avec TMD"])
+plt.xlabel("Temps (s)")
+plt.ylabel("Position (m)") #Vérifier l'unité et être sûr que c'est bien des courbes de position
+
+plt.show()
+
+#Format du .txt:
+#0;1;2   #Première ligne c'est les valeurs de temps, chaque fois séparé par un ;
+#2;3;4   #Deuxième ligne = sans TMD
+#2;4;5   #Troisième ligne = avec TMD
+
+#Fais attention à part faire un saut de ligne après la troisième ligne, sinon ça va buguer
+#Le fichier .txt doit être dans le même dossier que le .py et être renommé "donne.txt"
